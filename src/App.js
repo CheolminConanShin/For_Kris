4169f0cb65f5aa10f5443e1c88b236e418c1fd68
CheolminConanShin <scm0226@gmail.com>
2017-03-12 22:15:17 +0900

    Apply Theme to application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Background from './component/Background';
+
+injectTapEventPlugin();
 
 class App extends Component {
     render() {
         return (
-            <Background/>
+            <MuiThemeProvider>
+                <Background/>
+            </MuiThemeProvider>
         );
     }
 }
